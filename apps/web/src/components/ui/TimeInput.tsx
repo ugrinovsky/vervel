251d335e55972b2c63e4b8108d5f68735e64f917
nv.ugrinovskiy <nv.ugrinovskiy@tensor.ru>
2026-03-25 22:35:55 +0300

    feat(fixrs)

--- apps/web/src/components/ui/TimeInput.tsx
+++ apps/web/src/components/ui/TimeInput.tsx
@@ -1,3 +1,5 @@
+import { PatternFormat } from 'react-number-format';
+
 interface TimeInputProps {
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
@@ -5,15 +7,34 @@ interface TimeInputProps {
 }
 
 const BASE_CLS =
-  'w-full min-w-0 h-10 bg-(--color_bg_input) border border-(--color_border) rounded-xl px-3 text-white text-sm outline-none focus:border-(--color_primary_light) transition-colors appearance-none [&::-webkit-calendar-picker-indicator]:invert [&::-webkit-datetime-edit]:text-sm [&::-webkit-date-and-time-value]:text-left';
+  'w-full min-w-0 h-10 bg-(--color_bg_input) border border-(--color_border) rounded-xl px-3 text-white text-sm outline-none focus:border-(--color_primary_light) transition-colors appearance-none';
+
+const isTime = (val: string) => /^([01]\d|2[0-3]):[0-5]\d$/.test(val);
 
 export default function TimeInput({ value, onChange, className = '' }: TimeInputProps) {
+  const handleValueChange = (v: { formattedValue: string }) => {
+    const nextValue = v.formattedValue;
+    const output = nextValue.length === 5 && isTime(nextValue) ? nextValue : nextValue;
+    onChange({
+      target: { value: output } as unknown as EventTarget,
+    } as React.ChangeEvent<HTMLInputElement>);
+  };
+
   return (
-    <input
-      type="time"
+    <PatternFormat
       value={value}
-      onChange={onChange}
+      format="##:##"
+      placeholder="HH:MM"
+      mask="_"
       className={`${BASE_CLS} ${className}`}
+      onValueChange={handleValueChange}
+      onBlur={(e) => {
+        if (!isTime(e.currentTarget.value)) {
+          onChange({
+            target: { value } as unknown as EventTarget,
+          } as React.ChangeEvent<HTMLInputElement>);
+        }
+      }}
     />
   );
 }
