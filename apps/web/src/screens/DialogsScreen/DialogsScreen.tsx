0480c5b484a0c2eaef41c1ce396ee7471e5f4961
nv.ugrinovskiy <nv.ugrinovskiy@tensor.ru>
2026-03-29 15:27:00 +0300

    feat(chats)

--- apps/web/src/screens/DialogsScreen/DialogsScreen.tsx
+++ apps/web/src/screens/DialogsScreen/DialogsScreen.tsx
@@ -204,16 +204,15 @@ export default function DialogsScreen() {
             )}
           </motion.div>
         ) : (
-          filteredDialogs?.map((dialog, i) => (
-            <motion.div
-              key={dialog.chatId}
-              initial={{ opacity: 0, y: 10 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.2, delay: i * 0.04 }}
-            >
-              <DialogRow dialog={dialog} onOpen={() => openChat(dialog)} />
-            </motion.div>
-          ))
+          <motion.div
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.22 }}
+          >
+            {filteredDialogs?.map((dialog) => (
+              <DialogRow key={dialog.chatId} dialog={dialog} onOpen={() => openChat(dialog)} />
+            ))}
+          </motion.div>
         )}
       </motion.div>
     </Screen>
